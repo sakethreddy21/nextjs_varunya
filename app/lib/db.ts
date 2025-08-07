3a44312b8c6e94d0632065f1493af61ec6f3a2ad
Saketh Reddy <kk8429@srmist.edu.in>
2025-08-07 20:52:00 +0530

    chnages in db

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -5,13 +5,13 @@ let pool: Pool | null = null;
 
 export function getPool() {
   if (!pool) {
-    const password = Buffer.from(process.env.DB_PASSWORD_B64 || '', 'base64').toString('utf-8');
+    const password = Buffer.from(process.env.POSTGRES_PASSWORD_B64 || '', 'base64').toString('utf-8');
     pool = new Pool({
-      host: process.env.DB_HOST,
+      host: process.env.POSTGRES_HOST,
       port: Number(process.env.DB_PORT) || 5432,
-      user: process.env.DB_USER,
-      password:process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      user: process.env.POSTGRES_USER,
+      password:process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
     });
   }
   return pool;
