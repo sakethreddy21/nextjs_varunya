7fb293dd93ce0db195c7f194ae66f21ec28e4ca2
Saketh Reddy <kk8429@srmist.edu.in>
2025-10-01 20:52:25 +0530

    public

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://www.vaarunyaglobalexim.com/',
+  generateRobotsTxt: true,
+  // optional: change default changefreq and priority
+  // pagesDisabled: ['/404'],
+}
